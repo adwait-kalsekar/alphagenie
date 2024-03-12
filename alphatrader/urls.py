f090f7fec89315787c8da87be3b0bfdb3cac29d2
Adwait Mahesh Kalsekar <ak2521@scarletmail.rutgers.edu>
2024-03-12 17:39:13 -0400

    dashboard setup

--- alphatrader/urls.py
+++ alphatrader/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
 
+from . import views
+
+urlpatterns = [
+    path("login/", views.loginUser, name="login"),
+    path("signup/", views.signupUser, name="signup"),
+    path("dashboard/", views.index, name="dashboard"),
+]
--- alphatrader/views.py
+++ alphatrader/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'alphatrader/dashboard.html')
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'alphatrader/dashboard.html')
+        else:
+            return render(request, 'alphatrader/login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'alphatrader/login.html')
+    
+def logoutUser(request):
+    logout(request)
+    return render(request, 'alphatrader/login.html')
+
+def signupUser(request):
+    if request.method == 'POST':
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+        # email = request.POST.get('email')
+        # user = User.objects.create_user(username=username, password=password, email=email)
+        # user.save()
+        return render(request, 'alphatrader/signup.html')
+    else:
+        return render(request, 'alphatrader/signup.html')

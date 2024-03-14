34fa71c1b797e4b7706db36292662687e726ab2a
Adwait Mahesh Kalsekar <ak2521@scarletmail.rutgers.edu>
2024-03-14 14:10:47 -0400

    market dashboard setup

--- alphamarket/urls.py
+++ alphamarket/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="dashboard"),
     path("markets/", views.markets, name="markets"),
+    path("market-details/<str:ticker>", views.market_details, name="market_details"),
 
     # render 404 error page
     path("<path:all_path>/", views.errorPage, name="error"),
--- alphamarket/views.py
+++ alphamarket/views.py
@@ -12,8 +12,17 @@ def index(request):
 @login_required(login_url='login')
 def markets(request):
     page = "markets"
-    context = {"page": page}
-    return render(request, 'alphamarket/companies.html', context)
+    tickers = ["NVDA", "MSFT", "AAPL", "AMZN", "UNH", "BRK.B", "META", "LLY", "ORCL", "IBM"]
+    tickers_with_index = zip(range(len(tickers)), tickers)
+    context = {"page": page, "tickers": tickers_with_index}
+    return render(request, 'alphamarket/markets.html', context)
+
+@login_required(login_url='login')
+def market_details(request, ticker):
+    page = "markets"
+    
+    context = {"page": page, "ticker": ticker}
+    return render(request, 'alphamarket/market_details.html', context)
 
 @login_required(login_url='login')
 def errorPage(request, all_path):
